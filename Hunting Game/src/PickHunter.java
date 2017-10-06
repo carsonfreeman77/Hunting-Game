e0ed49720f57765265ab68585e44d1d6ea14d7cb
Carson.Freeman <Carson.Freeman@MU-C1-08.mullen.pvt.k12.co.us>
2017-10-06 08:25:19 -0600

    Fixed git

--- Hunting Game/src/PickHunter.java
+++ Hunting Game/src/PickHunter.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class PickHunter
 	{
@@ -7,17 +8,20 @@ public class PickHunter
 			{
 				setUpObjects();
 				askQuestions();
+				userBattle();
 
 			}
+
+		
 
 		private static void setUpObjects()
 			{
 
 				weaponP.add(new Weapon("Rifle", 500, ".308"));
-				weaponP.add(new Weapon("Shotfun", 40, "20 gauge"));
+				weaponP.add(new Weapon("Shotgun", 40, "20 gauge"));
 				weaponP.add(new Weapon("Compound Bow", 100, "Arrow"));
 				weaponP.add(new Weapon("Pistol", 150, ".45"));
-				weaponP.add(new Weapon("KNIFE!", 1, "K N I F E !"));
+				weaponP.add(new Weapon("Knife", 1, "K N I F E !"));
 				
 			}
 
@@ -26,14 +30,24 @@ public class PickHunter
 				Scanner in = new Scanner(System.in);
 				System.out.println("Please name your Hunter.");
 				String hunterName = in.nextLine();
+				
 				Scanner in2 = new Scanner(System.in);
-				System.out.println("From 0-100 what do you want your stealth to be...");
+				System.out.println("Welcome to the wilderness " + hunterName+ 
+						"!\nFrom 0-100 what do you want your stealth to be..."); //added greeting
+				
 				int hunterStealth = in2.nextInt();
-				System.out.println("Please pick your weapon...");
+				System.out.println("Please type in the weapon you desire...");
+				System.out.println();
+				
+				int counter = 1;
 				for (Weapon a: weaponP)
 					{
-						System.out.println(a.getWname() + " with a range of " + a.getRange() + " it also has " + a.getKind() + " as ammunition");
+
+						System.out.println(counter + ". "+ a.getWname() + " with a range of " + a.getRange() + " it also has " + a.getKind() + " as ammunition");
+						counter++;
 					}
+				
+				
 				String hunterWeapon = in.nextLine();
 				ArrayList <Hunter> hunterP = new ArrayList <Hunter>();
 				for (int i = 0; i < weaponP.size(); i++)
@@ -44,6 +58,24 @@ public class PickHunter
 							}
 					}
 				System.out.println(hunterP.get(0).getName() + " has a stealth level of " + hunterP.get(0).getStealth() + " and a " + hunterP.get(0).getWeapon().getWname() + " that has a range of " + hunterP.get(0).getWeapon().getRange() + " and has " + hunterP.get(0).getWeapon().getKind() + " as ammunition.");
+			}
+		private static void userBattle()  //coding the actual battle
+			{
+				System.out.println("Now you're all set, we will rise around 5:00am so get some rest...");
+				System.out.println();
+				
+					
+				try
+					{
+						Thread.sleep(1000);
+					} catch (InterruptedException e)
+					{
+						
+						e.printStackTrace();
+						
+					}
+					
+				
 			}
 
 	}
